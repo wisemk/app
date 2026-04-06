4f7ec0198237a0263dcdcd09294d6d548907ea9c
wisemk <kjr5164@gmail.com>
2026-04-07 01:14:51 +0900

    Use deployed Render content URL

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,7 +1,7 @@
 export const APP_CONFIG = {
   remoteContentUrl:
     process.env.EXPO_PUBLIC_REMOTE_CONTENT_URL ??
-    'https://example.com/soaek-bank-content.json',
+    'https://soaek-bank-content.onrender.com/api/content',
   contentSchemaVersion: 1,
   contentCacheKey: '@soaek-bank/remote-content-v1',
   contentRequestTimeoutMs: 6000,
